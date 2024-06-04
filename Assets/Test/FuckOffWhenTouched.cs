324340b56686a09052ee685e4734b4806b6023a9
BurnedFeathers <ralph.burgers2@gmail.com>
2024-06-04 23:25:38 +0200

    AAAAaAAA

--- Assets/Test/FuckOffWhenTouched.cs
+++ Assets/Test/FuckOffWhenTouched.cs
@@ -7,32 +7,10 @@ public class FuckOffWhenTouched : MonoBehaviour
     public GameObject DitObject;
 
 
-
-    //private void OnTriggerEnter(Collider Player)
-    //{
-    //    if (Collision.)
-    //    Debug.Log("Touched");
-    //
-    //    DitObject.GetComponent<MeshRenderer>().enabled = false;
-    //    DitObject.GetComponent<MeshCollider>().enabled = false;
-    //}
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.CompareTag("Player"))
-        {
-            Debug.Log("Touched " + DitObject);
-            
-            DitObject.GetComponent<MeshRenderer>().enabled = false;
-            DitObject.GetComponent<MeshCollider>().enabled = false;
-        }
-    }
-
     private void OnMouseDown()
     {
         Debug.Log("Clicked " + DitObject);
 
         DitObject.GetComponent<MeshRenderer>().enabled = false;
-        DitObject.GetComponent<MeshCollider>().enabled = false;
     }
 }
