e57197588ee47811fb9d4efd26dc01e951917600
BurnedFeathers <ralph.burgers2@gmail.com>
2024-06-05 13:34:16 +0200

    DE FUCKIN GDISSOLVE SHADER GODVERDOMME

--- Assets/Test/FuckOffWhenTouched.cs
+++ Assets/Test/FuckOffWhenTouched.cs
@@ -5,12 +5,49 @@ using UnityEngine;
 public class FuckOffWhenTouched : MonoBehaviour
 {
     public GameObject DitObject;
+    
+    public Material Material;
+    public float GaWegDanTime;
+    private bool GaDissolvenDan;
 
+    float x = 0;
+
+
+    private void Start()
+    {
+        DitObject.GetComponent<MeshRenderer>().material = Material;
+        GaDissolvenDan = false;
+        //DitObject.GetComponent<Renderer>().material.SetFloat("_Dissolve", -1);
+    }
+
+    private void FixedUpdate()
+    {
+        if (GaDissolvenDan == true)
+        {
+            x++;
+            float y = (float)((0.04167 * x) - 1);
+
+            DitObject.GetComponent<Renderer>().material.SetFloat("_Dissolve", y);
+        }
+    }
 
     private void OnMouseDown()
     {
         Debug.Log("Clicked " + DitObject);
 
-        DitObject.GetComponent<MeshRenderer>().enabled = false;
+        StartCoroutine(JeMoeder());
+
+        GaDissolvenDan = true;
     }
+
+    IEnumerator JeMoeder()
+    {
+        yield return new WaitForSeconds(GaWegDanTime);
+
+        //DitObject.GetComponent<MeshRenderer>().enabled = false;
+        GaDissolvenDan = false;
+    }
+
+
+
 }
